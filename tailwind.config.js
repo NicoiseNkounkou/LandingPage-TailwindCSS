79442d5765576dee32a054a5f839345b3058ddbb
Niçoise Nkounkou <nicoise.nkounkou@edu.devinci.fr>
2022-11-19 23:34:46 +0100

    font update

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,9 +8,15 @@ module.exports = {
         'cherry': "url('../images/mobile/image-graphic-design.jpg')",
         'fruit': "url('../images/mobile/image-photography.jpg')",
         'computerHeader': "url('../images/desktop/image-header.jpg')",
-      }
-
-    
+        'cherryComputer':"url('../images/desktop/image-graphic-design.jpg')",
+        'fruitComputer':"url('../images/desktop/image-photography.jpg')",
+      } ,
+     
+      fontFamily:  {
+        'barlow': ['Barlow', 'sans-serif'],
+        'fraunces': ['Fraunces','sans-serif']
+      },
+  
     },
   },
   plugins: [],
